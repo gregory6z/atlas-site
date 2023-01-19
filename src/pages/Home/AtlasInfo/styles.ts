e0e1dfd51125f9cdcafa9e278134035546564f45
gregory6z <gregoryrag@gmail.com>
2023-01-19 16:26:38 +0100

    1.0.19

--- src/pages/Home/AtlasInfo/styles.ts
+++ src/pages/Home/AtlasInfo/styles.ts
@@ -3,16 +3,27 @@ import styled from "styled-components"
 export const AtlasContainer = styled.div`
   width: 20%;
   text-align: center;
-  margin-top: 8%;
+  margin-top: 5%;
+
+  p {
+    font-size: 1.6rem;
+    font-weight: 500;
+  }
 
   h1 {
     margin-top: 1rem;
+    font-size: 2.5rem;
   }
 
   .horaires {
     color: ${(props) => props.theme["blue-500"]};
-    font-size: 1.5rem;
+
     margin-block: 0.5rem;
+
+    p {
+      font-size: 2rem;
+      font-weight: 700;
+    }
   }
 
   .imgContainer {
