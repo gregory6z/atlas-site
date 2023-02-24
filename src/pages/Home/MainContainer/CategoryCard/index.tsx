ef9eb04083841409c134a2ad1244125c634ab573
gregory6z <gregoryrag@gmail.com>
2023-02-24 09:38:34 +0100

    1.1.20

--- src/pages/Home/MainContainer/CategoryCard/index.tsx
+++ src/pages/Home/MainContainer/CategoryCard/index.tsx
@@ -2,8 +2,6 @@ import { CategoryCardContainer } from "../styles"
 
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import "react-lazy-load-image-component/src/effects/blur.css"
-import "react-lazy-load-image-component/src/effects/opacity.css"
-import "react-lazy-load-image-component/src/effects/black-and-white.css"
 
 interface CategoryCardProps {
   category?: string | null
@@ -20,7 +18,7 @@ export function CategoryCard({ category, image, link }: CategoryCardProps) {
         alt=""
         width="100%"
         height="85%"
-        effect="black-and-white"
+        effect="blur"
       />
     </CategoryCardContainer>
   )
